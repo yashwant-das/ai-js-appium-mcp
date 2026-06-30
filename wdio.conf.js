b18eea15ea81c94df1d31d6367aaef425d046b27
Yashwant Das <yashworks@gmail.com>
2026-06-30 21:45:52 +0530

    Add all dependencies for Appium/Wdio tests and create wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,39 @@
+exports.config = {
+  runner: 'local',
+  frameworks: ['mocha'],
+  reporters: ['spec'],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  },
+  capabilities: [
+    {
+      platformName: 'iOS',
+      'appium:deviceName': 'iPhone',
+      'appium:platformVersion': '18.0',
+      'appium:automationName': 'XCUITest',
+      'appium:udid': 'E60B3481-81CA-465F-8D62-AA87C48FAF3B',
+      'appium:bundleId': 'com.apple.reminders',
+      'appium:noReset': true,
+      'appium:waitForAppScript': 'true',
+      'appium:webDriverAgentUrl': 'http://127.0.0.1:56569'
+    }
+  ],
+  services: [['appium', {
+    command: 'appium',
+    args: {
+      relaxSecurityKey: 'true'
+    }
+  }]],
+  logLevel: 'info',
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 0,
+  services: [['appium', {
+    command: 'appium',
+    args: {
+      relaxSecurityKey: 'true'
+    }
+  }]]
+};
